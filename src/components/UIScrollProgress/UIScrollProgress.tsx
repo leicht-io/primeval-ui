5487e63b1bcb6116bbac813cb48d2cb1edacd98c
cle <cle@skov.com>
2021-10-02 18:43:10 +0200

    fix: renamed UIProgress to UIScrollProgress and refactored and optimized component.

---
+++ src/components/UIScrollProgress/UIScrollProgress.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './UIScrollProgress.scss';
+import {useResizeObserver, useScroll} from '../../@core';
+
+export const UIScrollProgress = React.memo((): React.ReactElement => {
+  const [value, setValue] = React.useState<number>(0);
+
+  React.useEffect(() => {
+    calculateProgress();
+  }, []);
+
+  useScroll(() => {
+    calculateProgress();
+  }, window);
+
+  useResizeObserver('.page-container', () => {
+    calculateProgress();
+  });
+
+  const calculateProgress = (): void => {
+    const documentElement: HTMLElement = document.documentElement;
+    const documentBody: HTMLElement = document.body;
+    const scrollTop: string = 'scrollTop';
+    const scrollHeight: string = 'scrollHeight';
+    const value = (documentElement[scrollTop] || documentBody[scrollTop]) / ((documentElement[scrollHeight] || documentBody[scrollHeight]) - documentElement.clientHeight) * 100;
+
+    setValue(value);
+  };
+
+  return <progress className="ui-scroll-progress" max={ 100 } value={ value } />;
+});
