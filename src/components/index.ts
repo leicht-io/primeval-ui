5e50f9b446b332bc52ae716f67385c0c086239f5
cle <cle@skov.com>
2021-10-02 18:43:45 +0200

    fix: updated SCSS to work with newest version with SASS.

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,7 @@ export * from './UIGallery';
 export * from './UIMenu';
 export * from './UITags';
 export * from './UIPageContainer';
-export * from './UIProgress';
+export * from './UIScrollProgress';
 export * from './UIDivider';
 export * from './UIHeader';
 export * from './UIIcon';
