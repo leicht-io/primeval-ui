3e888cfcc8a3a84dfdb5f8a88b7c5511721e748f
cle <christian@leicht.io>
2021-10-03 16:35:17 +0200

    refactor: refactored component.

--- src/components/UITypography/UITyphography.tsx
+++ src/components/UITypography/UITyphography.tsx
@@ -5,15 +5,15 @@ import {useClassNames} from '../../@core';
 
 export const UITypography = React.memo((props: IProps): React.ReactElement => {
   const GenericType = props.type;
-  const classNames = useClassNames('ui-typography', [
+  const classes: string = useClassNames('ui-typography', [
     [props.fontWeight, `ui-typography--fw${props.fontWeight}`],
     [props.skeleton, 'ui-typography--skeleton']
-  ], [props.skeleton, props.fontWeight]);
+  ]);
 
   return (
     <GenericType
       style={ {textAlign: props.textAlign || undefined} }
-      className={ classNames }>
+      className={ classes }>
       {props.children}
     </GenericType>
   );
